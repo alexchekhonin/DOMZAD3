c4c2b0b0064efb0d80631542f4c6c290dbeacbf3
Александр Чехонин <2007cheh@mail.ru>
2022-11-21 10:24:40 +0300

    zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два
+// числа и выдаёт, какое число большее, а какое меньшее.
+
+int a=-9;
+int b=-3;
+int Max = 0;
+int Min = 0;
+
+if (a>b)
+{
+Max = a;
+Min = b;
+}
+
+Max = b;
+Min = a;
+
+Console.WriteLine(Max);
+Console.WriteLine(Min);
